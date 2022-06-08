ffb3e1ac5f69713a69f92a682a0a7156f774fd25
lmly9193 <15520160+lmly9193@users.noreply.github.com>
2022-06-08 15:04:01 +0800

    update
    ## 請據脈絡詳述發生問題或變更理由
    #變更原因(Why):
    
    
    ## 請詳述您變更了什麼項目
    #變更項目(What):
    
    
    ## 若是變動原因含有發生問題請詳述您的解決做法
    #解決方案(How):
    
    
    ## 若是有發 Issue 請在這裡連結
    #Issue #ID
    
    
    # wip:         工作中的項目
    # feat:         新增/修改功能
    # fix:           修補臭蟲 # docs: 新增/修改文檔
    # refactor: 重構/優化結構
    # typo:        修正錯別字
    # style:       修改格式/風格
    # perf:        改善效能
    # test:         新增測試
    # chore:      新增/修改套件、雜事
    # secure:    修補漏洞/增加安全性
    # revert:     撤銷/回朔

---
+++ README.md
@@ -0,0 +1,56 @@
+<p align="center"><img src="https://i.imgur.com/by0dSN2.gif" width="400" title="來源: 我的邻居全是猫"></p>
+<p align="center">Hi, Im a Full-Stack Web Engineer. If you have any questions, please post a new <a href="https://github.com/lmly9193/lmly9193/discussions">discussion</a> here.</p>
+
+## Experiences
+
+<a href="https://github.com/anuraghazra/github-readme-stats">
+  <img align="right" src="https://github-readme-stats.vercel.app/api?username=lmly9193&count_private=true" width="400">
+</a>
+
+- Bachelor's degree in Optoelectronic Physics - 2019
+- Full-Stack Web Engineer
+
+<br><br><br><br>
+
+## Skills & Learning
+
+<a href="https://skillicons.dev"><img src="https://skillicons.dev/icons?i=nodejs,python,php,mysql,sqlite,laravel,html,js,css,bootstrap,jquery,nginx,linux,docker,aws,gcp,cloudflare,bash,discord,electron,firebase,git,github,gitlab,md,netlify,postman,redis,tailwind,vite,vscode,vue,workers"></a>
+
+Programming Languages:
+- Node.js
+- Python
+- PHP
+
+Databases:
+- MySQL (MariaDB)
+- Microsoft SQL Server
+- SQLite
+
+Back-End Development:
+- Laravel
+
+Front-End Development:
+- JavaScript
+- Bootstrap
+- CSS
+- jQuery
+
+Web Servers:
+- Apache
+- Nginx
+- IIS
+
+Server Operating Systems:
+- Ubuntu
+- Debian
+
+Server Management Tools:
+- Docker
+- WHM & CPanel
+
+Cloud Platforms:
+- AWS
+- GCP
+
+Serverless Platforms:
+- Cloudflare Workers
---
+++ bashrc.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+#=================================================
+# System: Debian/Ubuntu
+# Description: Bash Shell for Debian/Ubuntu
+# Author: lmly9193
+# Usage: curl -Lso- https://bit.ly/lmly9193 | bash && . ~/.bashrc
+#=================================================
+
+# 更新列表
+sudo apt update
+
+# 繁體中文語言包
+sudo apt install -y language-pack-zh-hant
+
+# 網路
+sudo apt install -y curl wget net-tools nmap ufw
+
+# 硬體
+sudo apt install -y htop neofetch virt-what
+
+# 壓縮
+sudo apt install -y zip unzip tar
+
+# 編輯器
+sudo apt install -y nano vim
+
+# Git
+sudo apt install -y git
+git config --global user.email "$(whoami)@$(hostname)"
+git config --global user.name "$(whoami)"
+
+# 一般
+sudo apt install -y screen
+
+# 設置顏色變量
+COLOR_RESET='\[\033[0m\]'          # 重置顏色為終端的預設值
+COLOR_GREEN='\[\033[01;32m\]'      # 綠色，粗體
+COLOR_BLUE='\[\033[01;34m\]'       # 藍色，粗體
+
+cat <<EOF >>~/.bashrc
+
+# ---------------- 使用者自訂 ----------------
+
+export TZ='Asia/Taipei'
+export LANG=zh_TW.UTF-8
+export LANGUAGE=zh_TW.UTF-8
+export LC_ALL=zh_TW.UTF-8
+export LS_OPTIONS='--time-style=long-iso --group-directories-first --color=auto'
+export VISUAL=nano
+export EDITOR=VISUAL
+export PS1='${COLOR_GREEN}\u@\h${COLOR_RESET}:${COLOR_BLUE}\w${COLOR_RESET}\$ '
+
+alias su='sudo -i'
+alias cls='clear'
+alias ls='ls -AF \${LS_OPTIONS}'
+alias ll='ls -lh'
+alias grep='grep --color=auto'
+alias rm='rm -iv'
+alias cp='cp -iv'
+alias mv='mv -iv'
+alias mkdir='mkdir -pv'
+alias rmdir='rm -r'
+alias df='df -h'
+alias du='du -hc'
+alias meminfo='free -mlt'
+alias cpuinfo='lscpu'
+alias pcinfo='neofetch'
+alias vminfo='virt-what'
+alias wget='wget -c'
+alias ping='ping -c 5'
+alias ports='netstat -tulanp'
+alias top='htop'
+alias untar='tar -zxvf'
+alias vi='vim'
+alias ipinfo='curl https://ipinfo.io/json && echo -e "\n"'
+alias bench='curl -Lso- bench.sh | bash'
+EOF
